be8864bc2d0cd21f79b23c213d9936af35732523
Invalidid404 <karl2003@naver.com>
2017-02-03 00:52:54 +0900

    Module Debug
    모듈 에러수정 : wid, hei를 따로 안구하고 그냥 5, 10으로 둬서 있었던 에러;; + 보고서 추가내용 등

--- pyGene.py
+++ pyGene.py
@@ -16,6 +16,14 @@ def minf(olist, func):
         if m>=func(olist[i]):
             olist.insert(0, olist[i].pop)
 
+def pick(a, b, p):
+    l = []
+    for _ in range(p):
+        l.append(a)
+    for _ in range(100-p):
+        l.append(b)
+    return random.choice(l)
+
 
 class Generation():
 
@@ -43,12 +51,12 @@ class Generation():
         return result                
     def cross_P(self, t, dad, mom): #���� ����
         pass    
-    def mutant(self,k, g): #��յ� ����(k = ���� Ȯ��)
-        result = [ [0]*5 for _ in range(10) ]
+    def mutant(self, g): #��յ� ����(k = ���� Ȯ��)
+        result = [ [0]*self.wid for _ in range(self.hei) ]
         d = lambda t, y: y*(1-r*((t+1)/self.Mg))
         for h in range(self.hei):
             for w in range(self.wid):
-                r = random.randint(0,1) #변이 할거야 안할거야
+                r = pick(0, 1, self.mutchance) #변이 할거야 안할거야
                 s = random.randint(0,1) #더하겠쏘!! 뺴겠쏘!!
                 if s==0 : result[h][w] = self.Generation[h][w]+math.floor(d(g, self.mutMax-self.Generation[h][w]))
                 elif s==1 : result[h][w] = self.Generation[h][w]-math.floor(d(g, self.Generation[h][w]-self.mutMin))
@@ -68,7 +76,7 @@ class Generation():
         self.mutMin = mutMin 
     def evol(self, k, t, g):
         self.Generation = self.offspring(k, t)
-        self.Generation = self.mutant(self.mutchance, g)
+        self.Generation = self.mutant(g)
         mxa = self.func(self.Generation[0])
         for i in range(self.len) :
             if mxa < self.func(self.Generation[i]) :
--- test.py
+++ test.py
@@ -2,19 +2,19 @@
 import pyGene
 import random
 
-olist = [0, 0, 0, 0, 0]
-gen = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)] for i in range(10)] 
+olist = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+gen = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)] for i in range(15)] 
 
 def func(l):
     f = 0
-    for i in range(5):
+    for i in range(10):
         if olist[i] == l[i]:
             f+=1
     return f
 #def __init__(self, GeneList, choice, crossover, mutantChance, mutMax, mutMin, func, MaxGeneration):
-Gen = pyGene.Generation(gen, 2, 1, 0, 1, 0, func, 100)
+Gen = pyGene.Generation(gen, 2, 1, 10, 1, 0, func, 200)
     
-for i in range(100):
+for i in range(200):
     g = Gen.evol(50, 50, i)[0]
-    print(g)
-    if g == 5 : break
+    print(func(g))
+    if func(g) == 10 : break
